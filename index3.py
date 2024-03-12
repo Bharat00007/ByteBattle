abe542c05f68c1d9fe1cdbd454fdcaf2db71754f
Bharat00007 <139622038+Bharat00007@users.noreply.github.com>
2024-03-12 22:17:23 +0530

    Update index3.py

--- index3.py
+++ index3.py
@@ -12,11 +12,11 @@ def find_indices_for_sum(nums, target):
             return [num_dict[complement], i]
         num_dict[num] = i
 
-# Take input from the user for the array
+
 user_input = input("Enter a list of numbers separated by spaces: ")
 nums = [int(x) for x in user_input.split()]
 
-# Take input from the user for the target sum
+
 target = int(input("Enter the target sum: "))
 
 result = find_indices_for_sum(nums, target)
