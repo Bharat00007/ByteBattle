532739594951513693a8b33e2b495685521cca96
Bharat00007 <139622038+Bharat00007@users.noreply.github.com>
2024-03-12 22:10:23 +0530

    Update index3.py

--- index3.py
+++ index3.py
@@ -1,3 +1,8 @@
+#Two Sum Problem: Given an array of integers, return indices of the two numbers such that they add 
+#up to a specific target. You may assume that each input would have exactly one solution, 
+#and you may not use the same element twice.
+
+
 def find_indices_for_sum(nums, target):
     num_dict = {}
 
